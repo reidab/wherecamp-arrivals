f574bc7595f014968f07e812493b6efa6e6f62a6
Reid Beels <mail@reidbeels.com>
2008-10-17 01:32:38 -0700

    Added beginning of shoes interface to arrivals.rb.
    Still need to finish and check in code that uses ArrivalsBoard to actually drive the shoes interface.

--- arrivals.rb
+++ arrivals.rb
@@ -1,5 +1,50 @@
+Shoes.setup do
+  gem 'google-geocode'
+end
+
+require 'google_geocode'
 require 'ticket'
 require 'ticket_pool'
 require 'arrivals_board'
+$board = ArrivalsBoard.new
 
-# do fun UI stuff
+Shoes.app(:title => "Control Panel") do
+
+  @controls = self
+  $board.window = window "Arrivals Board" do
+    $board.stack = stack
+  end
+  
+  stack do
+    para("Ticket Number", :size => 10).displace(0, 13)
+    @departure_number = edit_line :width => 100
+    @depart_button = button "Log Departure", :margin_top => 10
+    
+    @depart_button.click {
+      $board.window.app do
+        $board.stack.append { para 'test' }
+      end
+    }
+          
+    para("Ticket Number", :size => 10).displace(0, 13)
+    @arrival_number = edit_line :width => 100
+    
+    flow do
+      stack :width => 200 do
+        para("Origin", :size => 10).displace(0, 13)
+        @origin = edit_line
+      end
+      stack :width => 200, :margin_left => 20 do
+        para("Destination", :size => 10).displace(0, 13)
+        @destination = edit_line
+      end
+    end
+    
+    button "Log Arrival", :margin_top => 10 do
+
+    end
+    
+    @board_debug = para $board
+    
+  end
+end
--- arrivals_board.rb
+++ arrivals_board.rb
@@ -1,6 +1,8 @@
 # acts kind of like a controller between the Ticket model and the UI
 class ArrivalsBoard
   attr_accessor :tickets
+  attr_accessor :window
+  attr_accessor :stack
   
   def initialize
     @tickets = TicketPool.load
